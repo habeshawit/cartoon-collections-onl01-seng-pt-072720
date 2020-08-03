3211c66f7af83052ff1bbf905daebb7fd90e3162
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-03 01:09:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def long_planeteer_calls(calls)
   new_array = calls.map {|call| call}
-  new_array.any?{|i| i == true}
+  new_array.any?{|i| i.length > 4}
 end
 
 def find_the_cheese# code an argument here
