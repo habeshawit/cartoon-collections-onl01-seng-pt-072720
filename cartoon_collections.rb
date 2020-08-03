1534bf2122b8f88c63c88e40fa575446163dbe1d
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-03 01:17:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(calls)
   new_array.any?{|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(items)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
