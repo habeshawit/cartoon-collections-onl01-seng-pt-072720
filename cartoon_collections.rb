9fba4bfcaaf37e6a088a60ac51fb52f9a9da13b3
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-03 00:37:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,9 @@
 def roll_call_dwarves(names)
-  
   names.each_with_index {|name, index| puts "#{index+1}. #{name}"}
-   
-  
 end
 
 def summon_captain_planet(planeteer_calls)
-  
+  planeteer_calls.
 end
 
 def long_planeteer_calls# code an argument here
